cf52343a0f978e90730b96408765fb3f34110e6f
Alex Gutierrez <alex.techuba@gmail.com>
2016-03-02 13:37:00 -0800

    adding an icon

--- js/scripts.js
+++ js/scripts.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
   $("#quote-btn").on("click", function() {  
     $.getJSON("http://api.icndb.com/jokes/random?firstName=Gus&lastName=Bus", function(json) {
 
-      $(".quote").html(JSON.stringify(json.value.joke));  
+      $(".quote").html("<i class='fa fa-quote-left fa-2x'></i> " + json.value.joke);  
     });
   });
 });
